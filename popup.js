bb6a2f19f92f611724b3f160e3ff351c0e4a0494
savi <shambhavishandilya01@gmail.com>
2020-09-21 19:53:00 +0530

    Removed extra calls

--- popup.js
+++ popup.js
@@ -13,10 +13,6 @@ fetch("https://cricapi.com/api/cricket/Wh4CFtViqVbDCgaYC61AIw45UUM2")
 				var m2t = data.data[1].title;
 				m1title.textContent = m1t;
 				m2title.textContent = m2t;
-				uid1 = data.data[0].unique_id
-				uid2 = data.data[1].unique_id
-				getscore1(uid1);
-				getscore2(uid2);
 				
 				
 			})
@@ -24,36 +20,4 @@ fetch("https://cricapi.com/api/cricket/Wh4CFtViqVbDCgaYC61AIw45UUM2")
 		 var error = document.getElementById("error");
 		 error.textContent = "Error Ocuured!" 
 	  });
-}
-
-function getscore1(uid)
-{
-	var m1score = document.getElementById("m1score");
-	fetch("http://cricapi.com/api/cricketScore/Wh4CFtViqVbDCgaYC61AIw45UUM2?unique_id="+uid)
-	.then(response => response.json())
-            .then(data => {
-				var score = data.score;
-				m1score.textContent = score;
-				})
-			.catch(() => {
-		 var error = document.getElementById("error");
-		 error.textContent = "Error Ocuured!" 
-	  });
-				
-}
-
-function getscore2(uid)
-{
-var m2score = document.getElementById("m2score");
-	fetch("http://cricapi.com/api/cricketScore/Wh4CFtViqVbDCgaYC61AIw45UUM2?unique_id="+uid)
-	.then(response => response.json())
-            .then(data => {
-				var score = data.score;
-				m2score.textContent = score;
-				})
-			.catch(() => {
-		 var error = document.getElementById("error");
-		 error.textContent = "Error Ocuured!" 
-	  });
-				
 }
