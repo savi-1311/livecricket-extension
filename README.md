25834f6feae3293d0438e43af2dad7b67270a61f
savi <shambhavishandilya01@gmail.com>
2020-09-21 17:38:25 +0530

    Basics completed!

---
+++ README.md
@@ -0,0 +1,8 @@
+# weather-chrome-extension
+A chrome extension to find the live cricket scores using Cricbuzz API.
+Steps to run the extension:
+* Open chrome://extensions/ on your google chrome web browser.
+* Download the zip file of the repository and extract it (or just clone the repo).
+* Switch to development mode in the extensions tab.
+* Unpack the folder cloned.
+* Thats it,now run the extension on any of your tabs.
---
+++ popup.js
@@ -0,0 +1,59 @@
+window.onload = function()
+{
+	console.log("started");
+var m1title = document.getElementById("m1title");
+var m2title = document.getElementById("m2title");
+var uid1;
+var uid2;
+
+fetch("https://cricapi.com/api/cricket/Wh4CFtViqVbDCgaYC61AIw45UUM2")
+	.then(response => response.json())
+            .then(data => {
+				var m1t = data.data[0].title;
+				var m2t = data.data[1].title;
+				m1title.textContent = m1t;
+				m2title.textContent = m2t;
+				uid1 = data.data[0].unique_id
+				uid2 = data.data[1].unique_id
+				getscore1(uid1);
+				getscore2(uid2);
+				
+				
+			})
+	  .catch(() => {
+		 var error = document.getElementById("error");
+		 error.textContent = "Error Ocuured!" 
+	  });
+}
+
+function getscore1(uid)
+{
+	var m1score = document.getElementById("m1score");
+	fetch("http://cricapi.com/api/cricketScore/Wh4CFtViqVbDCgaYC61AIw45UUM2?unique_id="+uid)
+	.then(response => response.json())
+            .then(data => {
+				var score = data.score;
+				m1score.textContent = score;
+				})
+			.catch(() => {
+		 var error = document.getElementById("error");
+		 error.textContent = "Error Ocuured!" 
+	  });
+				
+}
+
+function getscore2(uid)
+{
+var m2score = document.getElementById("m2score");
+	fetch("http://cricapi.com/api/cricketScore/Wh4CFtViqVbDCgaYC61AIw45UUM2?unique_id="+uid)
+	.then(response => response.json())
+            .then(data => {
+				var score = data.score;
+				m2score.textContent = score;
+				})
+			.catch(() => {
+		 var error = document.getElementById("error");
+		 error.textContent = "Error Ocuured!" 
+	  });
+				
+}
